b2dfbae83ba12533a53e9d01ebaef4702eafa169
Tóth Ádám <tothadam.onp@gmail.com>
2021-07-30 11:52:22 +0200

    copy not relevant till deployment

--- 2.sh
+++ 2.sh
@@ -7,6 +7,6 @@ mkdir /root/.ssh
 ssh-keygen -f /etc/heketi/heketi_key -t rsa -N ''
 chown heketi:heketi /etc/heketi/heketi_key*
 
-for i in 10.42.0.1 10.42.1.1 10.42.2.1; do
-  ssh-copy-id -i /etc/heketi/heketi_key.pub root@$i
-done
+# for i in 10.42.0.1 10.42.1.1 10.42.2.1; do
+#   ssh-copy-id -i /etc/heketi/heketi_key.pub root@$i
+# done
