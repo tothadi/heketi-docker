73d26e2b8f55aba906de1c3258f1034387a927a5
Tóth Ádám <tothadam.onp@gmail.com>
2021-08-02 14:05:00 +0200

    sh1

--- 1.sh
+++ 1.sh
@@ -1,7 +1,10 @@
+cd /setup
 wget https://github.com/heketi/heketi/releases/download/v9.0.0/heketi-v9.0.0.linux.amd64.tar.gz
 tar -zxvf heketi-v9.0.0.linux.amd64.tar.gz
-chmod +x heketi/{heketi,heketi-cli}
-cp heketi/{heketi,heketi-cli} /usr/local/bin
+chmod +x heketi/heketi
+chmod +x heketi/heketi-cli
+cp heketi/heketi /usr/local/bin
+cp heketi/heketi-cli /usr/local/bin
 groupadd --system heketi
 useradd -s /sbin/nologin --system -g heketi heketi
 mkdir -p /var/lib/heketi /etc/heketi /var/log/heketi
